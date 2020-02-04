7f8c12b24f479d8741d2e8dfa1452e39de4af41a
Josue Rodriguez <smexyjosue@gmail.com>
2020-02-04 11:02:53 -0800

    style songlist component via reactstrap
    Used react-strap, css, and inline to style songlist cards in songlist
    component.

--- my-app/src/components/SongList.js
+++ my-app/src/components/SongList.js
@@ -20,19 +20,32 @@ const SongList = (props) => {
     console.log(`These the songs in the song list component`, props.songs);
   }, 1000);
   return (
-    <div className="cards">
-      <Card>
-        <CardHeader>{props.songs.artist_name}</CardHeader>
-        <CardBody>
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </CardText>
-          <Button>Play Song</Button>
-        </CardBody>
-        <CardFooter>Footer</CardFooter>
-      </Card>
+    <div className="card-container">
+      <div className="cards">
+        <Card>
+          <CardHeader
+            body
+            inverse
+            style={{ backgroundColor: "#333", borderColor: "#333" }}
+          >
+            Artist: {props.songs.artist_name}
+          </CardHeader>
+          <CardBody style={{ backgroundColor: "#3b3939", borderColor: "#333" }}>
+            <CardTitle>Song Name: {props.songs.song_name}</CardTitle>
+            <CardText>Duration: {props.songs.duration}</CardText>
+            <Button color="success" src={props.songs.song_url}>
+              Play Song
+            </Button>
+          </CardBody>
+          <CardFooter
+            body
+            inverse
+            style={{ backgroundColor: "#333", borderColor: "#333" }}
+          >
+            Genre: {props.songs.genre}
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 };
