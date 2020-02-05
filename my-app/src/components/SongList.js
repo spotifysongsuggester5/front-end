3185417e74ce1e9fb8c7e757971fb6f94bda7074
Hiter Harris <hiterharris@comcast.net>
2020-02-05 17:50:59 -0500

    delete request

--- my-app/src/components/SongList.js
+++ my-app/src/components/SongList.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { fetchData } from "../actions";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 import "../App.css";
 
 import { Button } from "reactstrap";
 
 const SongList = (props) => {
-
+  const [songs, setSongs] = useState([]);
+  console.log(props.songs);
   useEffect(() => {
     props.fetchData();
   }, []);
+
+  let id = props.songs.id;
+  const deleteFriend = () => {
+    axiosWithAuth()
+      .delete(`/api/songs/:id`, props.songs)
+      .then(response => {
+        setSongs(...response.data.filter(song => song.id !== id));
+      })
+  }
   
   return (
     <div className="card-container">
